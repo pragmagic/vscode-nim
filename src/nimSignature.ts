3461891d1dd8255a1a3e3758cb14fdaee3b27308
RSDuck <Kemal-Afzal@outlook.de>
2016-08-02 02:39:25 +0200

    Fixed infinte loop in the signature provider And improved the support for generics

--- src/nimSignature.ts
+++ src/nimSignature.ts
@@ -63,8 +63,6 @@ export class NimSignatureHelpProvider implements vscode.SignatureHelpProvider {
           identBeforeDot = line.substring(start, dotPosition);
         }
       }
-      console.log(identBeforeDot + " " + currentArgument);
-
 
       execNimSuggest(NimSuggestType.con, filename, position.line + 1, position.character - 1, getDirtyFile(document))
         .then(items => {
@@ -76,7 +74,20 @@ export class NimSignatureHelpProvider implements vscode.SignatureHelpProvider {
           items.forEach(item => {
             var signature = new vscode.SignatureInformation(item.type, item.documentation);
 
-            var signatureCutDown = /(proc|macro|template) \((([\w.]+: [\w.,;\[\] ]+([,;] )?)*)\)/.exec(item.type);
+            var genericsCleanType = "";
+            {
+              var insideGeneric = 0;
+              for (var i = 0; i < item.type.length; i++) {
+                if (item.type[i] == "[")
+                  insideGeneric++;
+                if (!insideGeneric)
+                  genericsCleanType += item.type[i];
+                if (item.type[i] == "]")
+                  insideGeneric--;
+              }
+            }
+
+            var signatureCutDown = /(proc|macro|template) \((.+: .+)*\)/.exec(genericsCleanType);
             var parameters = signatureCutDown[2].split(", ");
             parameters.forEach(parameter => {
               signature.parameters.push(new vscode.ParameterInformation(parameter));
