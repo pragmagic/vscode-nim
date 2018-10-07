c4af2e4a9cfe6aa982edb1a116bb2a70f7ccd565
technicallyagd <43739261+technicallyagd@users.noreply.github.com>
2018-10-07 21:33:03 +0800

    Auto-formatting fix (fixes issue #79) (#100)
    nimpretty actually formats code in-place now

--- src/nimFormatting.ts
+++ src/nimFormatting.ts
@@ -8,7 +8,6 @@
 import vscode = require('vscode');
 import cp = require('child_process');
 import fs = require('fs');
-import path = require('path');
 import { getDirtyFile, getNimPrettyExecPath } from './nimUtils';
 
 export class NimFormattingProvider implements vscode.DocumentFormattingEditProvider {
@@ -25,12 +24,10 @@ export class NimFormattingProvider implements vscode.DocumentFormattingEditProvi
         if (res.status !== 0) {
           reject(res.error);
         } else {
-          let ext = path.extname(file);
-          let prettyFile = path.join(path.dirname(file), path.basename(file, ext) + '.pretty' + ext);
-          if (!fs.existsSync(prettyFile)) {
-            reject(prettyFile + ' file not found');
+          if (!fs.existsSync(file)) {
+            reject(file + ' file not found');
           } else {
-            let content = fs.readFileSync(prettyFile, 'utf-8');
+            let content = fs.readFileSync(file, 'utf-8');
             let range = document.validateRange(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(1000000, 1000000)));
             resolve([vscode.TextEdit.replace(range, content)]);
           }
